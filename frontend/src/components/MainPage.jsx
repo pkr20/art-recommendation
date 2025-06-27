a17bdf725bfdea63a0aaafda9dbd503573fee14a
Kayleen Ramirez <pkr20@miami.edu>
2025-06-27 15:52:51 -0700

    changed api key

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -4,11 +4,37 @@ import { signOut } from 'firebase/auth';
 import { auth } from '../../../backend/api/firebase';
 import Card from './Card';
 import SearchBar from './SearchBar';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function MainPage() {
   const navigate = useNavigate();
   const [searchInput, setSearchInput] = useState('')
+  const [userLocation, setUserLocation] = useState('')
+  const [nearbyGalleries, setNearbyGalleries] = useState([])
+
+  //function to retrieve userLocation
+
+  //find nearby galleries using Google Places API
+
+  const findNearbyGalleries = async () => {
+    //if (!userLocation) return;
+    //setError('')
+    try {
+      const map = new google.maps.Map(document.createElement('div'));
+      const service = new google.maps.places.PlacesService(map);
+
+      const request = {
+        location: userLocation,
+        radius: '5000',
+        type: ['art_gallery', 'museum'],
+        keyword: 'art gallery mueseum'
+      }
+    } catch {
+      
+    }
+  }
+
+  
 
   //handle sign out from the main page
   const handleSignOut = async () => {
