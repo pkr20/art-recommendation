2903e48dbc34a84788e2cd4a39216d3117408550
Kayleen Ramirez <pkr20@miami.edu>
2025-06-30 12:31:22 -0700

    changed key

--- backend/api/firebase.js
+++ backend/api/firebase.js
@@ -11,15 +11,14 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 
-
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_KEY,
-  authDomain: "meta-dev-ad98e.firebaseapp.com",
-  projectId: "meta-dev-ad98e",
-  storageBucket: "meta-dev-ad98e.firebasestorage.app",
-  messagingSenderId: "195311860726",
-  appId: "1:195311860726:web:1529a37d211e9dd53fe5d7",
-  measurementId: "G-BZTPX1LWHY"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "meta-dev-f2fe4.firebaseapp.com",
+  projectId: "meta-dev-f2fe4",
+  storageBucket: "meta-dev-f2fe4.firebasestorage.app",
+  messagingSenderId: "848970558673",
+  appId: "1:848970558673:web:73baba9228d9aae5dfe9f0",
+  measurementId: "G-LN7K7FQXGZ"
 };
 
 // Initialize Firebase
