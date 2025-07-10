8d60f9bf419fe3c042eba1708f4058c3c3a5c417
Kayleen Ramirez <pkr20@miami.edu>
2025-07-10 15:36:27 -0700

    created map view for places

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,7 +1,15 @@
 import FavoritesPage from './FavoritesPage';
 import EditableInput from './EditableInput';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../../backend/api/firebase';
+import { useNavigate } from 'react-router-dom';
 
 export default function Profile() {
+    const navigate = useNavigate();
+    const handleSignOut = () => {
+        signOut(auth);
+        navigate('/signin');
+    }
     return (
         <div>
             <h1>Profile</h1>
@@ -11,8 +19,7 @@ export default function Profile() {
             <h2>To Visit Places: </h2>
             <h2>Reviews: </h2>
             <h2>Ratings: </h2>
-            <h2>Edit Profile</h2>
-            <h2>Sign Out</h2>
+            <button onClick={handleSignOut}>Sign Out</button>
         </div>
     )
 }
