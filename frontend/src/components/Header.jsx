60f14416ebae4a9b137b0b3f8a191341873f3d81
Kayleen Ramirez <pkr20@miami.edu>
2025-07-11 15:47:26 -0700

    css changes to the body

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,10 +5,9 @@ export default function Header({searchInput, setSearchInput}) {
     const navigate = useNavigate();
     return (
         <div className="header">
-            <h1>Exhibit Finder</h1>
+            <h1>ArtBase</h1>
             <SearchBar searchInput={searchInput} setSearchInput={setSearchInput} />
             <button className='header-btn' onClick={() => navigate('/signin')}>Sign In</button>
-            <button className='header-btn' onClick={() => navigate('/favorites')}>Favorites</button>
             <button className='header-btn' onClick={() => navigate('/profile')}>Profile</button>
             <button className='header-btn' onClick={() => navigate('/recommended')}>Recommended</button>
         </div>
