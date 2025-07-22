09b3830b0adfb1f041d023969fe69740da3a1387
Kayleen Ramirez <pkr20@miami.edu>
2025-07-21 20:34:28 -0700

    search ui typeahead

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import SearchBar from './SearchBar';
 import { useNavigate } from 'react-router-dom';
 import SignIn from './SignIn';
-export default function Header({searchInput, setSearchInput}) {
+export default function Header({searchInput, setSearchInput, places}) {
     const navigate = useNavigate();
     return (
         <div className="header">
             <h1>ArtBase</h1>
-            <SearchBar searchInput={searchInput} setSearchInput={setSearchInput} />
+            <SearchBar searchInput={searchInput} setSearchInput={setSearchInput} places={places}/>
             <button className='header-btn' onClick={() => navigate('/signin')}>Sign In</button>
             <button className='header-btn' onClick={() => navigate('/profile')}>Profile</button>
             <button className='header-btn' onClick={() => navigate('/recommended')}>Recommended</button>
